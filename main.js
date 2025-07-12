3e83945ccd778c105826833a70b4c2d30d52083e
HACHEM WAFAA <wafaahachem1431@gmail.com>
2025-07-12 14:16:40 +0100

    Use XMLHttpRequest to load and display users and posts

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getPosts(userId) {
   let request = new XMLHttpRequest();
   request.open(
     "GET",
-    "https://jsonplaceholder.typicode.com/posts?userId=" + userId
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
   );
   request.responseType = "json";
   request.send();
@@ -27,39 +27,41 @@ function getPosts(userId) {
 }
 
 function getUsers() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  let request = new XMLHttpRequest();
-  request.open("GET", "https://jsonplaceholder.typicode.com/users");
-  request.responseType = "json";
-  request.send();
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 300) {
-      let users = request.response;
-      document.getElementById("users").innerHTML = "";
-      for (user of users) {
-        let content = `
-          <div id="user" onclick = "userClicked(${user.id}, this)">
-            <h3>${user.name}</h3>
-            <h3>${user.email}</h3>
-          </div>`;
-        document.getElementById("users").innerHTML += content;
-      }
+  let request = new XMLHttpRequest(); //créer une requête HTTP (comme GET, POST, etc.) depuis JavaScript vers un serveur.
+  request.open("GET", 'https://jsonplaceholder.typicode.com/users'); // préparer une requête HTTP de type GET vers l’URL .
+  request.responseType = "json"; //recevoir automatiquement la réponse du serveur au format JSON.
+  request.send(); // → envoie la requête.
+  request.onload = function() {                     // 	Prépare une fonction pour plus tard, quand la réponse arrive
+    if(request.status >= 200 && request.status < 300){
+          let users = request.response;
+          document.getElementById("users").innerHTML = "";
+          for (user of users){
+            let content = `
+               <div id="user" onclick="userClicked(${user.id}, this)">
+                 <h3>${user.name}</h3>
+                 <h3>${user.email}</h3>
+               </div>
+               `
+            document.getElementById("users").innerHTML += content;   
+               ;
+          }
+
     } else {
       alert("error");
     }
-  };
-}
 
-function userClicked(id, el) {
-  getPosts(id);
-  let selectedElements = document.getElementsByClassName("selected");
-  for (element of selectedElements) {
-    element.classList.remove("selected");
   }
 
-  el.classList.add("selected");
 }
 
+getPosts(3);
 getUsers();
+
+function userClicked(id, ele) {
+  getPosts(id);
+  let selectedElements = document.getElementsByClassName("selected");
+  for(element of selectedElements){
+    element.classList.remove("selected");
+  }
+  ele.classList.add("selected");
+}
