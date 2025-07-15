f78987582e34e0d18980c940f306eaf2a050da39
HACHEM WAFAA <wafaahachem1431@gmail.com>
2025-07-15 13:30:22 +0100

    Fetch data from API

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 function getPosts(userId) {
-  let request = new XMLHttpRequest();
+
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+  .then(response => {
+    if(response.ok){
+      return response.json();
+    }
+  })
+  .then(posts => {
+    document.getElementById("posts").innerHTML = "";
+      for (post of posts) {
+        let content = `
+          <div id="post">
+            <h3>${post.title}</h3>
+            <h4>
+              ${post.body}
+            </h4>
+          </div>`;
+        document.getElementById("posts").innerHTML += content;
+      }
+  });
+  /*let request = new XMLHttpRequest();
   request.open(
     "GET",
     `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
@@ -23,18 +43,43 @@ function getPosts(userId) {
     } else {
       alert("error");
     }
-  };
+  };*/
 }
 
 function getUsers() {
-  let request = new XMLHttpRequest(); //créer une requête HTTP (comme GET, POST, etc.) depuis JavaScript vers un serveur.
-  request.open("GET", 'https://jsonplaceholder.typicode.com/users'); // préparer une requête HTTP de type GET vers l’URL .
-  request.responseType = "json"; //recevoir automatiquement la réponse du serveur au format JSON.
-  request.send(); // → envoie la requête.
-  request.onload = function() {                     // 	Prépare une fonction pour plus tard, quand la réponse arrive
-    if(request.status >= 200 && request.status < 300){
-          let users = request.response;
-          document.getElementById("users").innerHTML = "";
+  /* let request = new XMLHttpRequest(); //créer une requête HTTP (comme GET, POST, etc.) depuis JavaScript vers un serveur.
+  // request.open("GET", 'https://jsonplaceholder.typicode.com/users'); // préparer une requête HTTP de type GET vers l’URL .
+  // request.responseType = "json"; //recevoir automatiquement la réponse du serveur au format JSON.
+  // request.send(); // → envoie la requête.
+  // request.onload = function() {                     // 	Prépare une fonction pour plus tard, quand la réponse arrive
+  //   if(request.status >= 200 && request.status < 300){
+  //         let users = request.response;
+  //         document.getElementById("users").innerHTML = "";
+  //         for (user of users){
+  //           let content = `
+  //              <div id="user" onclick="userClicked(${user.id}, this)">
+  //                <h3>${user.name}</h3>
+  //                <h3>${user.email}</h3>
+  //              </div>
+  //              `
+  //           document.getElementById("users").innerHTML += content;   
+  //              ;
+  //         }
+
+  //   } else {
+  //     alert("error");
+  //   }
+
+  // }*/
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    if(response.ok){
+      return response.json();
+    }
+  })
+  .then(users => {
+    document.getElementById("users").innerHTML = "";
           for (user of users){
             let content = `
                <div id="user" onclick="userClicked(${user.id}, this)">
@@ -45,12 +90,7 @@ function getUsers() {
             document.getElementById("users").innerHTML += content;   
                ;
           }
-
-    } else {
-      alert("error");
-    }
-
-  }
+  });
 
 }
 
